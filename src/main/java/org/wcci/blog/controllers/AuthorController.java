e89c7076983fdb062add89d5993f223bac90c970
Sarah Greene <sgreene807@gmail.com>
2020-06-26 16:18:43 -0400

    author template and controller working

--- src/main/java/org/wcci/blog/controllers/AuthorController.java
+++ src/main/java/org/wcci/blog/controllers/AuthorController.java
@@ -1,11 +1,13 @@
 package org.wcci.blog.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.wcci.blog.storage.AuthorStorage;
 
+@Controller
 public class AuthorController {
 
     @Autowired
